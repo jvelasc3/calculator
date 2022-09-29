f1c73f73c51e74ec54e632e4a990f645fa220001
Jose A. Velasco <jvelasc3@uci.edu>
2022-09-28 17:33:23 -0700

    Added keyboard support

--- script.js
+++ script.js
@@ -93,3 +93,79 @@ function addDecimal() {
     currentValue += ".";
   }
 }
+
+window.onkeydown = function (e) {
+  e.preventDefault();
+  let x = e.key;
+  let choice;
+  switch (x) {
+    case "1":
+      choice = document.querySelector(".one");
+      choice.click();
+      break;
+    case "2":
+      choice = document.querySelector(".two");
+      choice.click();
+      break;
+    case "3":
+      choice = document.querySelector(".three");
+      choice.click();
+      break;
+    case "4":
+      choice = document.querySelector(".four");
+      choice.click();
+      break;
+    case "5":
+      choice = document.querySelector(".five");
+      choice.click();
+      break;
+    case "6":
+      choice = document.querySelector(".six");
+      choice.click();
+      break;
+    case "7":
+      choice = document.querySelector(".seven");
+      choice.click();
+      break;
+    case "8":
+      choice = document.querySelector(".eight");
+      choice.click();
+      break;
+    case "9":
+      choice = document.querySelector(".nine");
+      choice.click();
+      break;
+    case "0":
+      choice = document.querySelector(".zero");
+      choice.click();
+      break;
+    case "/":
+      choice = document.querySelector(".division");
+      choice.click();
+      break;
+    case "*":
+      choice = document.querySelector(".multiply");
+      choice.click();
+      break;
+    case "-":
+      choice = document.querySelector(".minus");
+      choice.click();
+      break;
+    case "+":
+      choice = document.querySelector(".plus");
+      choice.click();
+      break;
+    case ".":
+      choice = document.querySelector(".decimal");
+      choice.click();
+      break;
+    case "Enter":
+      choice = document.querySelector(".equal");
+      choice.click();
+      break;
+    case "Backspace":
+      choice = document.querySelector("#clear-btn");
+      choice.click();
+      break;
+  }
+};
